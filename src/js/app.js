880dbac06f252ef00afd5db8472660a1452240a5
MyCode07 <erem19992015@mail.ru>
2023-06-13 14:40:28 +0300

    start

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+// import './utils/smoothscrol.js';
+// import './utils/menu.js';
+// import './utils/animations.js';
+
+// import './utils/mouse.js';
+// import './utils/ticker.js';
+// import './utils/to-top.js';
+// import './utils/popup.js';
+// import './utils/inputmask.js';
+// import './utils/forms.js';
+// import './utils/accordeon.js';
+// import './utils/replace.js';
